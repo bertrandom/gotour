299624822e711b0aea9aff856e0e62b10186cf97
Bertrand Fan <bertrand@fan.net>
2014-10-05 19:16:42 -0700

    exercise 25

---
+++ 25.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func Sqrt(x float64) float64 {
+
+	var last, second_to_last float64
+
+	z := float64(2.0)
+
+	for i := 0; i < 1000; i++ {
+
+		z = z - (z*z-x)/(2*z)
+
+		if second_to_last == z || last == z{
+			return z
+		}
+
+		second_to_last = last
+		last = z
+
+	}
+
+
+	return z
+
+}
+
+func main() {
+
+	fmt.Println(math.Sqrt(7))
+	fmt.Println(Sqrt(7))
+
+}
